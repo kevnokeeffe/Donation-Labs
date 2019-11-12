ad3b903ac4bec1e8b3dfbed0a1d2c51057fb3ab0
Kevin O Keeffe <kevokeeffe@gmail.com>
2019-11-12 11:12:32 +0000

    Updated vue donation

--- DonationVue 2.0/src/components/Donations.vue
+++ DonationVue 2.0/src/components/Donations.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="hero">
     <h3 class="vue-title"><i class="fa fa-list" style="padding: 3px"></i>{{messagetitle}}</h3>
-    <p>{{donations}}</p>
+    <div id="app1">
+      <v-client-table :columns="columns" :data="donations" :options="options">
+      </v-client-table>
+    </div>
   </div>
 </template>
 
 <script>
     import DonationService from '@/services/donationservice'
+    import Vue from 'vue'
+    import VueTables from 'vue-tables-2'
+
+    Vue.use(VueTables.ClientTable, {compileTemplates: true, filterByColumn: true})
     export default {
         name: 'Donations',
         data () {
             return {
                 messagetitle: ' Donations List ',
                 donations: [],
-                errors: []
+                errors: [],
+                columns: ['_id', 'paymenttype', 'amount', 'upvotes'],
+                options: {
+                    headings: {
+                        _id: 'ID',
+                        paymenttype: 'Payment Type',
+                        amount: 'Amount',
+                        upvotes: 'Upvotes'
+                    }
+                }
             }
         },
         // Fetches Donations when the component is created.
@@ -46,4 +62,9 @@
     font-size: 45pt;
     margin-bottom: 10px;
   }
+  #app1 {
+    width: 60%;
+    margin: 0 auto;
+  }
+
 </style>
--- DonationVue 2.0/src/services/donationservice.js
+++ DonationVue 2.0/src/services/donationservice.js
@@ -3,5 +3,9 @@ import Api from '@/services/api'
 export default {
   fetchDonations () {
     return Api().get('/donations/all')
+  },
+  postDonation (donation) {
+    return Api().post('/donations', donation,
+      { headers: {'Content-type': 'application/json'} })
   }
 }
